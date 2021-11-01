0d1200676cdfdb5b204de7d8f84e0d83afa43d83
mbbrug <matthew.brugman@utexas.edu>
2021-11-01 04:39:54 -0500

    replaced body part category id with string name

--- index.js
+++ index.js
@@ -23,26 +23,46 @@ app.get('/exercise', (req, res) => {
 
 async function wgr_request(req) {
     try {
+
+
+        var cat = req.query.category;
+        var cat_id = 10;
+
+        if(cat == 'Abs'){
+            cat_id =10;
+        }else if( cat == 'Arms'){
+            cat_id=8;
+        }else if( cat == 'Back'){
+            cat_id=12;
+        }else if( cat == 'Calves'){
+            cat_id=14;
+        }else if( cat == 'Chest'){
+            cat_id=11;
+        }else if( cat == 'Legs'){
+            cat_id=9;
+        }else if( cat == 'Shoulders'){
+            cat_id=13;
+        };
     
-        let result = await axios.get(`https://wger.de/api/v2/exercise/?language=2&limit=10&category=${req.query.category}`);
+        let result = await axios.get(`https://wger.de/api/v2/exercise/?language=2&limit=30&category=${cat_id}`);
         let data = result.data;
         //console.log(req.query);
         //console.log(`https://wger.de/api/v2/exercise/?language=2&limit=100&category=${req.query.category}`)
 
         var res_arr=result.data.results;
 
-        res_arr.forEach((arrObj) => {
-            delete arrObj['uuid'];
-            delete arrObj['exercise_base'];
-            delete arrObj['creation_date'];
-            delete arrObj['category'];
-            delete arrObj['equipment'].id;
-            delete arrObj['muscles_secondary'];
-            delete arrObj['license'];
-            delete arrObj['language'];
-            delete arrObj['license_author'];
-            delete arrObj['variations'];
-          });
+        // res_arr.forEach((arrObj) => {
+        //     delete arrObj['uuid'];
+        //     delete arrObj['exercise_base'];
+        //     delete arrObj['creation_date'];
+        //     delete arrObj['category'];
+        //     delete arrObj['equipment'].id;
+        //     delete arrObj['muscles_secondary'];
+        //     delete arrObj['license'];
+        //     delete arrObj['language'];
+        //     delete arrObj['license_author'];
+        //     delete arrObj['variations'];
+        //   });
 
 
         res.send(res_arr);
